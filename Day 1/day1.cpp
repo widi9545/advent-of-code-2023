f6ffd954c9361f3d73da2a4506dd40cfaf59202a
widi9545 <widi9545@colorado.edu>
2023-12-17 13:25:38 -0700

    getting ready to sum up number characters

--- Day 1/day1.cpp
+++ Day 1/day1.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std; 
 
 class numberCount{
     public:
-        vector<string> line;
-        int firstNumber;
-        int secondNumber;
+        vector<string> lines;
+        int total;
+        int fileLen;
 
         int sumNumbers(int firstNumber, int secondNumber){
+            return 1;
+        }
+
+        void returnFileLen(){
+            for(int i = 0; i < lines.size(); i ++){
+                fileLen = i;
+
+            }
+        }
+
+    
+
+        int iterateVector(){
+            int firstNumber;
+            int secondNumber;
+
+            for(auto & line : lines){
+                for(auto & character : line){
+                    if(isdigit(character)){
+                        cout << character << endl;
+                    }
+                }
+            }
             return 1;
         }
 
@@ -19,7 +43,24 @@ class numberCount{
 
 
 int main(){
-    cout << "Hello World!" << endl;
+    string line;
+    string fileName = "day1.txt";
+    ifstream file(fileName);
+
+    numberCount numberCounter;
+
+    if(file.is_open()){
+        while(getline(file, line)){
+            // cout << line << endl;
+            numberCounter.lines.push_back(line);
+        }
+        file.close();
+    }
+
+    numberCounter.returnFileLen();
+    numberCounter.iterateVector();
+
+
 
     return 1;
 }
