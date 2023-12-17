d2bccf3e79c6472606ba380d5a4de5ea01e3bcf7
William Diment <widi9545@colorado.edu>
2023-12-17 12:43:44 -0700

    first commit with day 1 essentials

---
+++ Day 1/day1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std; 
+
+class numberCount{
+    public:
+        vector<string> line;
+        int firstNumber;
+        int secondNumber;
+
+        int sumNumbers(int firstNumber, int secondNumber){
+            return 1;
+        }
+
+};
+
+
+
+int main(){
+    cout << "Hello World!" << endl;
+
+    return 1;
+}
